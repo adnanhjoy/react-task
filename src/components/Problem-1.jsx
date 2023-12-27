283e93cda8adade864a5dc7427719d823de8d3fe
engadnanhossain <engr.adnanhossain@gmail.com>
2023-12-27 23:21:31 +0600

    solve problem one

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,12 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
-
+    const [userData, setUserData] = useState([]);
     const [show, setShow] = useState('all');
+    const activeUserData = userData.filter((user) => user.status.toLowerCase() === "active");
+    const completedUserData = userData?.filter((user) => user?.status.toLowerCase() === "completed");
 
-    const handleClick = (val) =>{
+
+    const handleClick = (val) => {
         setShow(val);
     }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const status = e.target.status.value;
+        const data = {
+            name,
+            status
+        }
+        setUserData([...userData, data]);
+        e.target.reset();
+    }
+
+
+    function sortUserDataByStatus(a, b) {
+        const statusOrder = { active: 1, completed: 2, pending: 3, other: 4 };
+        return statusOrder[a.status.toLowerCase()] - statusOrder[b.status.toLowerCase()];
+    }
+
+    const sortedUserData = userData.sort(sortUserDataByStatus);
 
     return (
 
@@ -14,12 +38,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form onSubmit={handleSubmit} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" className="form-control" placeholder="Name" name='name' />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" className="form-control" placeholder="Status" name='status' />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,26 +53,50 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>Completed</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
-                        <tbody>
-                        
-                        </tbody>
+                        {
+                            show === 'all' &&
+                            sortedUserData?.map((user, idx) =>
+                                <tr key={idx}>
+                                    <td>{user?.name}</td>
+                                    <td>{user?.status}</td>
+                                </tr>
+                            )
+                        }
+                        {
+                            show === 'active' &&
+                            activeUserData?.map((user, idx) =>
+                                <tr key={idx}>
+                                    <td>{user?.name}</td>
+                                    <td>{user?.status}</td>
+                                </tr>
+                            )
+                        }
+                        {
+                            show === 'completed' &&
+                            completedUserData?.map((user, idx) =>
+                                <tr key={idx}>
+                                    <td>{user?.name}</td>
+                                    <td>{user?.status}</td>
+                                </tr>
+                            )
+                        }
                     </table>
                 </div>
             </div>
